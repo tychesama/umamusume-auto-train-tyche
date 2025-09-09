87ea0ef7075bc7354a502a1bd357a5551ec9d29c
Seçkin Ozan Uyan <ozanuyan@gmail.com>
2025-09-09 20:46:23 +0300

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -18,16 +18,3 @@ def drag_scroll(bottomMousePos, to):
   pyautogui.moveRel(0, to, duration=0.25)
   pyautogui.mouseUp()
   pyautogui.click()
-
-'''
-def drag_scroll(bottomMousePos, topMousePos):
-  if not topMousePos or not bottomMousePos:
-    error("drag_scroll correct variables not supplied.")
-  pyautogui.moveTo(bottomMousePos, duration=0.1)
-  pyautogui.mouseDown()
-  pyautogui.moveTo(topMousePos, duration=0.4)
-  pyautogui.moveTo((topMousePos[0], topMousePos[1]+20), duration=0.1)
-  pyautogui.moveTo((topMousePos[0], topMousePos[1]+10), duration=0.1)
-  pyautogui.moveTo((topMousePos[0], topMousePos[1]+15), duration=0.1)
-  pyautogui.mouseUp()
-'''
